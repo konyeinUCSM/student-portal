07828c11dd87e688f9928e30970f97e9898b0546
Aung Ko Nyein <aungkonyein548@gmail.com>
2026-03-13 21:02:21 +0630

    no message

--- student-portal/src/main/java/com/manulife/studentportal/repository/UserRepository.java
+++ student-portal/src/main/java/com/manulife/studentportal/repository/UserRepository.java
@@ -18,5 +18,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
+    boolean existsByEmailAndIdNot(String email, Long id);
+
     Page<User> findByRole(Role role, Pageable pageable);
 }
